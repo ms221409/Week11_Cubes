36556136e51a630cd1713a5d8807b7ebeb9d961f
Michael Stephens <ms221409@ohio.edu>
2019-03-26 17:39:48 -0400

    Initial commit

---
+++ CUBES/Assets/Scripts/CubeInstance.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInstance : MonoBehaviour
+{
+    public float life = 5;
+    public Rigidbody rb;
+
+    float _elapsedLife = 0;
+
+
+    void OnEnable()
+    {
+        _elapsedLife = 0;
+        rb.velocity = Vector3.up * 2;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        _elapsedLife = AddLife ();
+
+
+
+        if (_elapsedLife > life)
+        {
+            Debug.Log(GetRandomCompliment ());
+
+
+
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            gameObject.SetActive(false);
+
+            //Destroy(gameObject);
+        }
+    }
+
+
+    float AddLife ()
+    {
+        _elapsedLife += Time.deltaTime;
+
+        return _elapsedLife;
+    }
+
+
+    string GetRandomCompliment ()
+    {
+        string compliment = "";
+        float randomNum = Random.value;
+
+        if (randomNum < 0.1f)
+        {
+            compliment = "You look nice!";
+        }
+        else if (randomNum < 0.2f)
+        {
+            compliment = "Good Work!";
+        }
+        else if (randomNum < 0.3f)
+        {
+            compliment = "Nice shoes!";
+        }
+
+        return compliment;
+    }
+}
---
+++ CUBES/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject [] cubePool;
+    public int poolSize = 100;
+
+
+
+
+    public GameObject cubePrefab;
+    //public Text cubeCountText;
+    //public int cubesPerFrame = 10;
+
+    //int _currentCubeCount = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCubePool();
+    }
+
+
+    void SpawnCubePool ()
+    {
+        cubePool = new GameObject[poolSize];
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject thisCube = Instantiate (cubePrefab, transform.position, Quaternion.identity);
+            cubePool [i] = thisCube;
+            thisCube.SetActive(false);
+
+            thisCube.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        }
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < cubePool.Length; i++)
+        {
+            if (!cubePool [i].activeSelf)
+            {
+                cubePool[i].SetActive (true);
+                cubePool[i].transform.position = transform.position;
+
+                break;
+            }
+        }
+    }
+}
